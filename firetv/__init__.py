c3423977cb8f8dc89418b90ba5ecdd67ce357480
dan.diodati <dan.diodati@veeva.com>
2021-11-14 21:32:08 -0800

    DEV-master need to connect twice to a device

--- firetv/__init__.py
+++ firetv/__init__.py
@@ -347,11 +347,17 @@ class FireTV:
                     logging.debug("Trying to connect to device: ip: %s, port: %d", device_ip, device_port)
 		    
                     self._adb_device = self._adb_client.remote_connect(host=device_ip, port=device_port)
-                    self._available = bool(self._adb_device)
 
+                    if not bool(self._adb_device) 
+                      logging.debug("Trying to connect second time to device: ip: %s, port: %d", device_ip, device_port)
+                      self._adb_device = self._adb_client.remote_connect(host=device_ip, port=device_port)
+
+                    self._available = bool(self._adb_device)
+                    logging.debug("Device available: %s", str(self._available))
+                      
                 except:
                     try:
-                      logging.debug("Trying to connect second time to device: ip: %s, port: %d", device_ip, device_port)
+                      logging.debug("Exception connecting again to device: ip: %s, port: %d", device_ip, device_port)
                       self._adb_device = self._adb_client.remote_connect(host=device_ip, port=device_port)
                       self._available = bool(self._adb_device)
                     except Exception as e:
